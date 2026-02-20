e236a24b3af944a6c1d9e4e18c247cadee56ca9d
Romain <benetiere.romain@gmail.com>
2026-02-20 18:56:32 +0100

    Track custom_components and other config folders

---
+++ custom_components/rfplayer/const.py
@@ -0,0 +1,42 @@
+"""Constants for the rfplayer integration."""
+DOMAIN = "rfplayer"
+DATA_RFOBJECT = "rfobject"
+
+CONF_AUTOMATIC_ADD = "automatic_add"
+CONF_RECONNECT_INTERVAL = "reconnect_interval"
+
+DEFAULT_RECONNECT_INTERVAL = 10
+DEFAULT_SIGNAL_REPETITIONS = 1
+
+PLATFORMS = ["sensor", "switch", "number"]
+
+ATTR_EVENT = "event"
+
+RFPLAYER_PROTOCOL = "rfplayer_protocol"
+
+CONF_DEVICE_ADDRESS = "device_address"
+CONF_FIRE_EVENT = "fire_event"
+CONF_IGNORE_DEVICES = "ignore_devices"
+CONF_SIGNAL_REPETITIONS = "signal_repetitions"
+
+DATA_DEVICE_REGISTER = "device_register"
+DATA_ENTITY_LOOKUP = "entity_lookup"
+
+CONNECTION_TIMEOUT = 10
+
+EVENT_BUTTON_PRESSED = "button_pressed"
+EVENT_KEY_COMMAND = "command"
+EVENT_KEY_ID = "id"
+EVENT_KEY_SENSOR = "sensor"
+EVENT_KEY_UNIT = "unit"
+
+RFPLAYER_GROUP_COMMANDS = ["allon", "alloff"]
+
+SERVICE_SEND_COMMAND = "send_command"
+
+SIGNAL_AVAILABILITY = "rfplayer_device_available"
+SIGNAL_HANDLE_EVENT = "rfplayer_handle_event_{}"
+SIGNAL_EVENT = "rfplayer_event"
+
+COMMAND_ON = "ON"
+COMMAND_OFF = "OFF"
---
+++ custom_components/rfplayer/sensor.py
@@ -0,0 +1,127 @@
+"""Support for Rfplayer sensors."""
+import logging
+
+from homeassistant.const import CONF_DEVICES
+from homeassistant.helpers.entity import EntityCategory
+
+from . import RfplayerDevice
+from .const import (
+    CONF_AUTOMATIC_ADD,
+    DATA_DEVICE_REGISTER,
+    DATA_ENTITY_LOOKUP,
+    DOMAIN,
+    EVENT_KEY_ID,
+    EVENT_KEY_SENSOR,
+    EVENT_KEY_UNIT,
+)
+from .rflib.rfpparser import PACKET_FIELDS, UNITS
+
+_LOGGER = logging.getLogger(__name__)
+
+SENSOR_ICONS = {
+    "humidity": "mdi:water-percent",
+    "battery": "mdi:battery",
+    "temperature": "mdi:thermometer",
+}
+
+
+def lookup_unit_for_sensor_type(sensor_type):
+    """Get unit for sensor type.
+
+    Async friendly.
+    """
+    field_abbrev = {v: k for k, v in PACKET_FIELDS.items()}
+
+    return UNITS.get(field_abbrev.get(sensor_type))
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up the Rfplayer platform."""
+    config = entry.data
+    options = entry.options
+
+    # add jamming entity
+    async_add_entities([RfplayerJammingSensor()])
+
+    async def add_new_device(device_info):
+        """Check if device is known, otherwise create device entity."""
+        device_id = device_info[EVENT_KEY_ID]
+
+        # create entity
+        device = RfplayerSensor(
+            protocol=device_id.split("_")[0],
+            device_id=device_id.split("_")[1],
+            unit_of_measurement=device_info[EVENT_KEY_UNIT],
+            initial_event=device_info,
+        )
+        _LOGGER.debug("Add sensor entity %s", device_id)
+        async_add_entities([device])
+
+    if CONF_DEVICES in config:
+        for device_id, device_info in config[CONF_DEVICES].items():
+            if EVENT_KEY_SENSOR in device_info:
+                await add_new_device(device_info)
+
+    if options.get(CONF_AUTOMATIC_ADD, config[CONF_AUTOMATIC_ADD]):
+        hass.data[DOMAIN][DATA_DEVICE_REGISTER][EVENT_KEY_SENSOR] = add_new_device
+
+
+class RfplayerSensor(RfplayerDevice):
+    """Representation of a Rfplayer sensor."""
+
+    def __init__(
+        self,
+        protocol,
+        device_id=None,
+        unit_of_measurement=None,
+        initial_event=None,
+        name=None,
+        **kwargs,
+    ):
+        """Handle sensor specific args and super init."""
+        self._protocol = protocol
+        self._device_id = device_id
+        self._attr_name = name
+        self._attr_unit_of_measurement = unit_of_measurement
+        super().__init__(
+            protocol, device_id=device_id, initial_event=initial_event, name=name
+        )
+
+    async def async_added_to_hass(self):
+        """Register update callback."""
+        # Register id and aliases
+        await super().async_added_to_hass()
+
+        if self._initial_event:
+            self.hass.data[DOMAIN][DATA_ENTITY_LOOKUP][EVENT_KEY_SENSOR][
+                self._initial_event[EVENT_KEY_ID]
+            ] = self.entity_id
+
+    def _handle_event(self, event):
+        """Domain specific event handler."""
+        self._state = event["value"]
+
+    @property
+    def state(self):
+        """Return value."""
+        return self._state
+
+
+class RfplayerJammingSensor(RfplayerDevice):
+    """Representation of a Jamming Rfplayer sensor."""
+
+    def __init__(self):
+        """Handle sensor specific args and super init."""
+        self._attr_entity_category = EntityCategory.DIAGNOSTIC
+        super().__init__(
+            "JAMMING", device_id=0, name="Jamming detection"
+        )
+
+    def _handle_event(self, event):
+        """Domain specific event handler."""
+        self._state = event["value"]
+
+    @property
+    def state(self):
+        """Return value."""
+        return self._state
